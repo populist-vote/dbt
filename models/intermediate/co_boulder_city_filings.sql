8a6c100131afca7aa57a777e76152be2b25bcbfc
Jon Biggerstaff <jonbig04@gmail.com>
2023-10-27 09:12:11 -0700

    working on creating boulder city pols and offices

--- models/intermediate/co_boulder_city_filings.sql
+++ models/intermediate/co_boulder_city_filings.sql
@@ -1,22 +1,22 @@
 WITH transformed_filings AS (
     SELECT
-        email as email,
-        first_name as first_name,
-        last_name as last_name,
-        official_website_url as official_website_url,
-        home_state as home_state,
-        office as office,
-        office as office_title,
-        'city wide' as district,
+        email AS email,
+        first_name AS first_name,
+        last_name AS last_name,
+        official_website_url AS official_website_url,
+        home_state AS home_state,
+        office AS office,
+        office AS office_title,
+        'city wide' AS district,
         'Boulder' AS county,
-        CONCAT(first_name, ' ', last_name) as candidate_full_name,
+        CONCAT(first_name, ' ', last_name) AS candidate_full_name,
         CASE
             WHEN office ILIKE '%City Council%' THEN 'City Council'
-            ELSE NULL 
         END AS political_scope
 
     FROM p6t_state_co.boulder_updated_filings
 ),
+
 transformed_filings_1 AS (
     SELECT
         email,
@@ -27,65 +27,41 @@ transformed_filings_1 AS (
         office,
         candidate_full_name,
         political_scope,
-        slugify(candidate_full_name) AS politician_slug,
+        'city' AS district_type,
+        SLUGIFY(candidate_full_name) AS politician_slug,
         CASE
-            WHEN office ILIKE '%City Council%' OR office ILIKE '%Mayor%' THEN 'city'
+            WHEN
+                office ILIKE '%City Council%' OR office ILIKE '%Mayor%'
+                THEN 'city'
             ELSE 'district'
         END AS election_scope,
-        CASE 
-            WHEN office ILIKE '%Mayor%' then 'Mayor'
+        CASE
+            WHEN office ILIKE '%Mayor%' THEN 'Mayor'
             ELSE 'At Large'
-        END AS seat,
-        'city' AS district_type
+        END AS seat
 
     FROM transformed_filings
 )
+
 SELECT
-    tf.*, 
+    tf.*,
     tf1.politician_slug,
-    tf1.politician_slug as slug,
+    tf1.politician_slug AS slug,
     tf1.election_scope,
     tf1.district_type,
     tf1.seat,
-    slugify(tf.home_state || ' ' || tf.office_title || ' ' || tf.county || ' ' || tf.district || ' ' || tf1.seat) AS office_slug
-FROM 
+    SLUGIFY(
+        tf.home_state
+        || ' '
+        || tf.office_title
+        || ' '
+        || tf.county
+        || ' '
+        || tf.district
+        || ' '
+        || tf1.seat
+    ) AS office_slug
+FROM
     p6t_state_co.boulder_updated_filings AS f
 LEFT JOIN transformed_filings AS tf ON f.email = tf.email
 LEFT JOIN transformed_filings_1 AS tf1 ON tf.email = tf1.email
-
-
---fields for politician table
-----Where is the id field coming from?
---politician_id as id
-
-
-
-
-
-
---fields needs for office table:
--- SELECT DISTINCT ON(office_slug)
--- office_id AS id,
-
--- office_slug AS ref_key,
--- office_slug as slug, 
-    ---- create office slug
-    -- slugify(CONCAT('MN', ' ', f.office_title, ' ', f.county,  ' ', f.district , ' ', f.seat)) AS office_slug,
--- state::state,
--- state_id,
--- office_title AS title,
--- seat,
--- district,
--- political_scope::political_scope,
--- election_scope::election_scope,
--- district_type::district_type,
--- county
-     
-
--- create office slug
--- slugify(CONCAT('MN', ' ', f.office_title, ' ', f.county,  ' ', f.district , ' ', f.seat)) AS office_slug,
-
-
-
-
-
