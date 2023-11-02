03ab04896b8e69f14f4e39e1dd4dc25f6f1c7bc4
Wiley Conte <wileymckayconte@gmail.com>
2023-11-02 18:53:14 -0400

    Lint and clean up

--- models/staging/stg_co_boulder_city_offices.sql
+++ models/staging/stg_co_boulder_city_offices.sql
@@ -1,24 +1,15 @@
-SELECT DISTINCT ON(office_slug)
-    --office_id AS id,
-	office_slug AS ref_key,
-	office_slug as slug, 
-    home_state as state,
-    --state_id,
-	office_title AS title,
-	seat,
-	district,
-	--school_district,
-    political_scope as political_scope,
-	election_scope as election_scope,
-	district_type as district_type,
-	'Boulder' as municipality,
-	county as county
+SELECT DISTINCT ON (office_slug)
+    office_slug AS ref_key,
+    office_slug AS slug,
+    home_state AS state,
+    office_title AS title,
+    seat,
+    district,
+    school_district,
+    political_scope AS political_scope,
+    election_scope AS election_scope,
+    district_type AS district_type,
+    'Boulder' AS municipality,
+    county AS county
 FROM
-	{{ ref('co_boulder_city_filings') }}
-
-
--- INSERT INTO office (ref_key, slug, state, state_id, title, seat, district, school_district, political_scope, election_scope, district_type, municipality, county) (
--- 	SELECT
--- 		ref_key, slug, state, state_id, title, seat, district, school_district, political_scope, election_scope, district_type, municipality, county
--- 	FROM
--- 		dbt_wiley.stg_offices
+    {{ ref('co_boulder_city_filings') }}
--- models/staging/stg_co_boulder_city_politicians.sql
+++ models/staging/stg_co_boulder_city_politicians.sql
@@ -1,16 +1,9 @@
 SELECT DISTINCT ON (politician_slug)
-    --politician_id as id,
-    politician_slug as ref_key,
-    politician_slug as slug,
-	first_name,
-	last_name,
-	email,
+    politician_slug AS ref_key,
+    politician_slug AS slug,
+    first_name,
+    last_name,
+    email,
     home_state
 FROM
-	{{ ref ('co_boulder_city_filings') }}
-
-
--- INSERT INTO politician (ref_key, slug, first_name, middle_name, last_name, suffix, preferred_name, phone, email, home_state) 
--- 	SELECT
--- 		ref_key, slug, first_name, middle_name, last_name, suffix, preferred_name, phone, email, home_state	
--- 	FROM dbt_wiley.stg_mn_sos_county_politicians WHERE id IS null;
+    {{ ref ('co_boulder_city_filings') }}
