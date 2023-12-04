08a4ba291d1df16f73dff7ef138734b2d5aa84b2
Wiley Conte <wileymckayconte@gmail.com>
2023-12-04 10:39:06 -0600

    WIp on legiscan federal data

--- models/intermediate/legiscan_federal_bills.sql
+++ models/intermediate/legiscan_federal_bills.sql
@@ -2,26 +2,26 @@ WITH transformed_bills AS (
     SELECT
         title,
         description,
-        bill_id AS legiscan_bill_id,
+        bill_id::integer AS legiscan_bill_id,
         session_id AS legiscan_session_id,
         bill_number,
         status_desc,
         status_date,
-        committee_id AS legiscan_committee_id,
+        committee_id::integer AS legiscan_committee_id,
         committee AS legiscan_committee,
-        last_action_date AS legiscan_last_action_date,
+        last_action_date::date AS legiscan_last_action_date,
         last_action AS legiscan_last_action,
         url AS full_text_url,
         state_link,
-        'federal' AS political_scope,
-        SLUGIFY(CONCAT('us', '-', bill_number, '-', '2023-2024')) AS bill_slug,
+        'federal'::political_scope AS political_scope,
         COALESCE((
             JSON_BUILD_OBJECT(
                 1, 'introduced', 2, 'in_consideration', 4, 'became_law'
             )::jsonb
         )
         ->> status, 'introduced'
-        ) AS bill_status,
+        )::bill_status AS status,
+        SLUGIFY(CONCAT('us', '-', bill_number, '-', '2023-2024')) AS slug,
         (
             SELECT id
             FROM session
---
+++ models/staging/federal/stg_federal_bills.sql
@@ -0,0 +1,15 @@
+SELECT
+    title,
+    description,
+    legiscan_bill_id,
+    bill_number,
+    legiscan_committee_id,
+    legiscan_committee,
+    legiscan_last_action_date,
+    legiscan_last_action,
+    full_text_url,
+    political_scope,
+    slug,
+    status,
+    session_id
+FROM {{ ref('legiscan_federal_bills') }}
